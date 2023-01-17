faa7064fb4bd43c7cbc170a8124718c068b47bdf
DazedDeer <coolsheep22@gmail.com>
2023-01-17 14:43:35 +1300

    Tidied the Settings page

--- qy7IsuTDT8WI876pymKb_Assignment_3_StudentStarterCode.zip_expanded/Assignment 3 StudentStarterCode/Settings.java
+++ qy7IsuTDT8WI876pymKb_Assignment_3_StudentStarterCode.zip_expanded/Assignment 3 StudentStarterCode/Settings.java
@@ -1,22 +1,34 @@
-
+// Class containing the game's settings
 public class Settings {
+	
+	// Window settings
 	public static final String WINDOW_NAME = "Breakout";
 	public static final int WINDOW_WIDTH = 400;
 	public static final int WINDOW_HEIGHT = 500;
+	
+	// Brick settings
 	public static final int TOTAL_BRICKS = 20;
 	public static final int BRICK_VERT_PADDING = 50;
 	public static final int BRICK_HEIGHT = 30;
 	public static final int BRICK_HORI_PADDING = 50;
 	public static final int BRICK_WIDTH = 70;
+	
+	// Ball settings
 	public static final int INITIAL_BALL_X = 150;
 	public static final int INITIAL_BALL_Y = 350;
+	public static final int BALL_WIDTH = 10;
+	public static final int BALL_HEIGHT = 10;
+	
+	// Paddle settings
 	public static final int INITIAL_PADDLE_X = 100;
 	public static final int INITIAL_PADDLE_Y = 370;
 	public static final int PADDLE_WIDTH = 100;
 	public static final int PADDLE_HEIGHT = 15;
-	public static final int BALL_WIDTH = 10;
-	public static final int BALL_HEIGHT = 10;
+	
+	// Message's position setting
 	public static final int MESSAGE_POSITION = 270;
+	
+	// Lives position settings
 	public static final int LIVES_POSITION_X = 5;
 	public static final int LIVES_POSITION_Y = 15;
 }
