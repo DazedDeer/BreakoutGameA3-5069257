95ffb0c9eefcac8b0c5df1ec47474295a54b42bb
DazedDeer <coolsheep22@gmail.com>
2023-01-17 15:40:13 +1300

    Finished Paddle Class

--- qy7IsuTDT8WI876pymKb_Assignment_3_StudentStarterCode.zip_expanded/Assignment 3 StudentStarterCode/Paddle.java
+++ qy7IsuTDT8WI876pymKb_Assignment_3_StudentStarterCode.zip_expanded/Assignment 3 StudentStarterCode/Paddle.java
@@ -1,33 +1,60 @@
 import java.awt.Graphics;
 
+// The Paddle Class which is a child of the Sprite Class
 public class Paddle extends Sprite {
 
+	
+	// The paddle's xVelocity attribute
 	private int xVelocity;
 	
+	
+	// Paddle constructor
 	public Paddle() {
-		// TODO: Set width to Settings.PADDLE_WIDTH
-		// TODO: Set width to Settings.PADDLE_HEIGHT
-		// TODO: Call resetPosition
+		
+		// Set the paddle's height and width
+		this.width = Settings.PADDLE_WIDTH;
+		this.height = Settings.PADDLE_HEIGHT;
+		
+		// Reset the paddle's position to the initial position
+		resetPosition();
 	}
 	
+	
+	// Resets the paddle's position to the initial position
 	public void resetPosition() {
-		// TODO: Set initial position x and y (use INITIAL_PADDLE_X/Y)
-		// Note: Check Ball.java for a hint
+		
+		// The paddle is set to the initial x and y positions from Settings
+		this.x = Settings.INITIAL_PADDLE_X;
+		this.y = Settings.INITIAL_PADDLE_Y;
 	}
 	
+	
+	// Updates the paddle's movement by increasing the paddle's x attribute by the paddle's xVelocity
 	public void update() {
+		
+		// Update the paddle's x attribute by adding the paddle's xVelocity to it
 		x += xVelocity;
 		
-		// TODO: Prevent the paddle from moving outside of the screen
-		// This can be done using two if statements (one for the left side of the screen and one for the right)
-
+		// Prevents the paddle from moving outside the screen on the left side
+		if (this.getX() == this.getWidth()/2) {
+			this.setX(this.getX() + 1);
+		}
+		
+		// Prevents the paddle from moving outside the screen on the right side
+		else if (this.getX() == (Settings.WINDOW_WIDTH - this.getWidth()/2)) {
+			this.setX(this.getX() - 1);
+		}
 	}
 	
+	
+	// Paints the paddle's graphics
 	public void paint(Graphics g) {
 		g.fillRect(x, y, Settings.PADDLE_WIDTH, Settings.PADDLE_HEIGHT);
 	}
 	
+	
+	// Sets the paddle's xVelocity
 	public void setXVelocity(int vel) {
-		// TODO: Set x velocity
+		this.xVelocity = vel;
 	}
 }
