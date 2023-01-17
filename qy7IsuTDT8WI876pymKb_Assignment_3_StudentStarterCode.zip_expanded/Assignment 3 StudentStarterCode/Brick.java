4904ace867ce131428b50733f9ae7b67492467fa
DazedDeer <coolsheep22@gmail.com>
2023-01-17 15:51:09 +1300

    Finished the Brick Class

--- qy7IsuTDT8WI876pymKb_Assignment_3_StudentStarterCode.zip_expanded/Assignment 3 StudentStarterCode/Brick.java
+++ qy7IsuTDT8WI876pymKb_Assignment_3_StudentStarterCode.zip_expanded/Assignment 3 StudentStarterCode/Brick.java
@@ -1,22 +1,40 @@
 import java.awt.Graphics;
 
+
+// The Brick Class which is a child of the Sprite Class
 public class Brick extends Sprite {
 	
+	
+	// Brick attribute for whether the brick is broken or now (false = not broken, true = broken)
 	private boolean broken = false;
 	
+	
+	// Brick constructor
 	public Brick(int x, int y) {
-		// TODO: Set x using the parameter
-		// TODO: Set y using the parameter
-		// TODO: Set the width and height of the brick using Settings.BRICK_WIDTH/HEIGHT
+		
+		// Sets the brick's size 
+		this.width = Settings.BRICK_WIDTH;
+		this.height = Settings.BRICK_HEIGHT;
+		
+		// Sets the brick's x and y position
+		this.x = x;
+		this.y = y;
 	}
 
+	
+	// Method that tells you whether the brick is broken or not
 	public boolean isBroken() {
-		return false;	// TODO: Return the correct variable
-	}
-	public void setBroken(boolean b) {
-		// TODO: Set the broken variable using the parameter given
+		return this.broken;	
 	}
 	
+	
+	// Method that sets whether the brick is broken or not
+	public void setBroken(boolean broken) {
+		this.broken = broken;
+	}
+	
+	
+	// Paints the Brick graphics
 	public void paint(Graphics g) {
 		if(!broken) {
 			g.fillRect(x, y, Settings.BRICK_WIDTH, Settings.BRICK_HEIGHT);
